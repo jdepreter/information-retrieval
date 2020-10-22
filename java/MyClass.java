5c333bfbfca180c55f8611b288fb245910e9ed9f
jdepreter <jolan.depreter@gmail.com>
2020-10-22 11:05:02 +0200

    added java lucene

---
+++ java/MyClass.java
@@ -0,0 +1,16 @@
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.index.CorruptIndexException;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+import org.apache.lucene.util.Version;
+
+
+public class MyClass {
+    public static void main(String[] args) {
+      System.out.println("Hello World");
+      System.out.println(Version.LATEST);
+    }
+  }
